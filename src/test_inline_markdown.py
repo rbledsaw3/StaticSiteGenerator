d66605f7f5de334ee3017dc626f24fb85dd47aed
Robert Bledsaw III <rbledsaw3@outlook.com>
2024-11-09 13:43:18 -0500

    added functions and test to extract markdown links and images

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,8 @@
 import unittest
 from inline_markdown import (
     split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
 )
 
 from textnode import TextNode, TextType
@@ -65,7 +67,7 @@ class TestInlineMarkdown(unittest.TestCase):
         node = TextNode("**bold** and *italic*", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
         new_nodes = split_nodes_delimiter(new_nodes, "*", TextType.ITALIC)
-        self.assertListEqual(
+        self.assertEqual(
             [
                 TextNode("bold", TextType.BOLD),
                 TextNode(" and ", TextType.TEXT),
@@ -86,6 +88,35 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes,
         )
 
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+                "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+                "This is text with a [link](https://www.google.com)"
+        )
+        self.assertListEqual([("link", "https://www.google.com")], matches)
+
+    def test_extract_multiple_markdown_links(self):
+        matches = extract_markdown_links(
+                "This is text with a [link](https://www.google.com) and [another](https://www.yahoo.com)"
+        )
+        self.assertListEqual(
+            [("link", "https://www.google.com"), ("another", "https://www.yahoo.com")],
+            matches,
+        )
+
+    def test_extract_multiple_markdown_images(self):
+        matches = extract_markdown_images(
+                "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and ![another](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual(
+            [("image", "https://i.imgur.com/zjjcJKZ.png"), ("another", "https://i.imgur.com/zjjcJKZ.png")],
+            matches,
+        )
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -10,7 +10,6 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
-
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
